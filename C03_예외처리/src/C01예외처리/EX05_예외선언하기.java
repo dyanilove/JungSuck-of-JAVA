7592b512b51f212154be218d54f0f6debebb456e
dyanilove <ekdms216@nate.com>
2023-11-28 23:32:35 +0900

    day12

---
+++ C03_예외처리/src/C01예외처리/EX05_예외선언하기.java
@@ -0,0 +1,11 @@
+package C01예외처리;
+
+public class EX05_예외선언하기 {
+	public static void main(String[] args) {
+//		예외 처리하는 방법 : try-catch문, 예외 선언하기
+//		메서드가 호출시 발생가능한 예외를 호출하는 쪽에 알리는 것.
+//		※예외를 발생시키는 키워드 throw, 예외를 메서드에 선언하는 키워드 throws 구별!
+		
+	}
+
+}
