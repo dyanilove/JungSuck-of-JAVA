431e824b706f7380ec20fd3b648640d744c624b9
dyanilove <ekdms216@nate.com>
2023-11-29 00:37:41 +0900

    day 12

--- C03_예외처리/src/C01예외처리/EX05_예외선언하기.java
+++ C03_예외처리/src/C01예외처리/EX05_예외선언하기.java
@@ -6,6 +6,26 @@ public class EX05_예외선언하기 {
 //		메서드가 호출시 발생가능한 예외를 호출하는 쪽에 알리는 것.
 //		※예외를 발생시키는 키워드 throw, 예외를 메서드에 선언하는 키워드 throws 구별!
 		
+//		finally블럭
+//			: 예외 발생여부와 관계없이 수행되어야 하는 코드를 넣는다.
+		
+//		예외 되던지기
+//			: 예외를 처리한 후에 다시 예외를 발생시키는 것.
+//			: 호출한 메서드와 호출된 메서드 양쪽 모두에서 예외처리하는 것.
+		try {
+			method1();
+		}catch(Exception e) {
+			System.out.println("main메서드에서 예외처리 되었습니다.");
+		}
+		
+	}
+	static void method1() throws Exception{
+		try {
+			throw new Exception();
+		} catch (Exception e) {
+			System.out.println("method1메서드에서 예외 처리 되었습니다.");
+			throw e;	// 다시 예외를 발생시킨다.
+		}
 	}
 
 }
---
+++ C03_예외처리/src/C01예외처리/EX06_연결된예외.java
@@ -0,0 +1,13 @@
+package C01예외처리;
+
+public class EX06_연결된예외 {
+	
+//	연결된 예외(chained exception)
+//		: 한 예외가 다른 예외를 발생시킬 수 있다.
+//		: 예외 A가 예외 B를 발생시키면, A는 B의 원인 예외
+	
+//	연결된 예외를 하는 이유
+//		1. 여러 예외를 하나로 묶어서 다루기 위해
+//		2. checked예외(Exception자손, 필수처리)를 unchecked예외(RuntimeException자손, 선택처리)로 변경하려 할 때
+
+}
